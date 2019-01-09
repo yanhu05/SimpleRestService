f8f8001bb4440ad55d347b5c8e3fc47c21ed7209
Yan Hu <yan.hu@ipreo.com>
2019-01-09 18:36:20 -0500

    Save work

--- SimpeRestService/src/main/java/com/example/demo/service/SimpleService.java
+++ SimpeRestService/src/main/java/com/example/demo/service/SimpleService.java
@@ -1,10 +1,12 @@
 package com.example.demo.service;
 
+import com.example.demo.dao.CompanyDao;
 import com.example.demo.dao.EmployeeDao;
 import com.example.demo.model.Employee;
 
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,8 +19,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class SimpleService {
 	
 	@Autowired 
-	
 	EmployeeDao employeeDao;
+	
+	@Autowired 
+	CompanyDao companyDao;
 	
 	@GetMapping(value = "/rest/employee/{id}")
     public Employee getEmployeeById(@PathVariable Long id) {
@@ -37,5 +41,15 @@ public class SimpleService {
 		System.out.println(newEmployee.getSalary());
 		employeeDao.insert(newEmployee);
 		return newEmployee;
+    }
+	
+    @GetMapping(value = "/rest/companies")
+    public List<String> getAllCompanyNames() {
+		return companyDao.getCompanyNames();
+    }
+    
+    @GetMapping(value = "/rest/company/{name}")
+    public BigDecimal getCompanyCost(@PathVariable String name) {
+		return companyDao.getCompanyCost(name);
     }
 }
